652ed0561ec4c500b1f724f071384edc79de0162
blaize.chuang <blaize.chuang@ileopard.com>
2017-10-19 16:30:28 +0800

    Add solution for BinaryGap

--- Solution.java
+++ Solution.java
@@ -19,4 +19,24 @@ public class Solution {
 
         return array;
     }
+
+    public int Binarygap(int N) {
+        String binary = Integer.toBinaryString(N);
+        int i = binary.length() - 1;
+        while(binary.charAt(i) == '0') {
+            i--;
+        }
+
+        int gap = 0;
+        int counter = 0;
+        for (; i >= 0; i--) {
+            if (binary.charAt(i) == '1') {
+                gap = Math.max(gap, counter);
+                counter = 0;
+            } else {
+                counter++;
+            }
+        }
+        return gap;
+    }
 }
